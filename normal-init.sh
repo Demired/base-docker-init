0f13860eebaa3ef7ece61e8e25afd1b12c39682d
0X8C <nevermakeyoucrytoli@gmail.com>
2019-10-10 14:24:48 +0800

    Update normal-init.sh

--- normal-init.sh
+++ normal-init.sh
@@ -14,9 +14,7 @@ cat ~/.ssh/id_rsa.pub
 
 read -p "请打开 https://github.com/settings/keys 添加一个新key"
 
-systemctl docker start
-
-systemctl docker enable
+service docker start
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
